4f928cfea70dfa947e6cb96730641945f800588c
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-08 18:39:00 +0300

    контакты сохраняются в localStorage

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,33 @@
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 import Contact from '../Contact/Contact';
-const ContactList = ({ contacts, onDelete }) => (
-  <ul>
-    {contacts.map(contact => (
-      <Contact
-        key={contact.id}
-        contact={contact}
-        onDelete={onDelete}
-        id={contact.id}
-      />
-    ))}
-  </ul>
-);
+class ContactList extends Component {
+  componentDidMount() {
+    const { initialContact } = this.props;
+    const contacts = localStorage.getItem('contacts');
+    initialContact(JSON.parse(contacts));
+  }
+  componentDidUpdate(prevProps, _) {
+    if (prevProps.contacts !== this.props.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.props.contacts));
+    }
+  }
+  render() {
+    const { contacts, onDelete } = this.props;
+    return (
+      <ul>
+        {contacts.map(contact => (
+          <Contact
+            key={contact.id}
+            contact={contact}
+            onDelete={onDelete}
+            id={contact.id}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
 export default ContactList;
 ContactList.propType = {
   contacts: PropTypes.exact({
